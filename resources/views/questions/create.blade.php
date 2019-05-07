64f0cc5dc69700fc8c9936ec4d6703c53adaa03d
DESKTOP-OQ5ORD0\sajib <sajib.k.mridha@gmail.com>
2019-05-07 13:22:51 +0600

    Implement form validation and store the question

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -15,20 +15,21 @@
                 </div>
 
                 <div class="card-body">
+
                   <form action="{{ route('questions.store')}}" method="post">
                       @csrf
                       <div class="form-group">
                           <label for="question-tile">Question Title</label>
-                          <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') }}">
+                          <input type="text" name="title" id="question-title" value="{{ old('title') }}"  class="form-control {{ $errors->has('title') ?  'is-invalid' : '' }}">
                           @if($errors->has('title'))
                             <div class="invalid-feedback">
-                                <strong>{{ $errors->first('title') ?  'is-invalid' : '' }}</strong>
+                                <strong>{{ $errors->first('title') }}</strong>
                             </div>
                           @endif
                       </div>
                       <div class="form-group">
                           <label for="question-body">Explain your questin</label>
-                          <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ?  'is-invalid' : '' }}"></textarea>
+                          <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ?  'is-invalid' : '' }}">{{ old('body') }}</textarea>
                           @if($errors->has('body'))
                             <div class="invalid-feedback">
                                 <strong>{{ $errors->first('body') }}</strong>
